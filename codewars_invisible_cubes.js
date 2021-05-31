4593431714a73d7856cbd7cd99956728b1eeade7
Crew Spence <csspence123@gmail.com>
2021-05-31 05:48:34 -0500

    Wrote working solution.

--- codewars_invisible_cubes.js
+++ codewars_invisible_cubes.js
@@ -1,11 +1,9 @@
 /*
-Imagine there's a big cube consisting of n3 n^3n 
-3
-  small cubes. Calculate, how many small cubes are not visible from outside.
+Imagine there's a big cube consisting of n^3 small cubes. Calculate, how many small cubes are not visible from outside.
 
 For example, if we have a cube which has 4 cubes in a row, then the function should return 8, because there are 8 cubes inside our cube (2 cubes in each dimension)
 */
 
-function notVisibleCubes(n) {
-  // your code!
+const notVisibleCubes = (n) => {
+  return Math.max(0, (n-2)**3);
 }
